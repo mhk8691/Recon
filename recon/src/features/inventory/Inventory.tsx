d744f2877ccab9a4d10cead4829e2cff84c0e6e0
mhk8691 <mohammadhosseinkaboli86@gmail.com>
2024-09-21 01:28:34 +0330

    fix bugs

--- recon/src/features/inventory/Inventory.tsx
+++ recon/src/features/inventory/Inventory.tsx
@@ -1,13 +1,36 @@
-import { Container } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import Search from "./Search";
 import SortFilter from "./Sort-Filter";
 import Table from "./Table";
 export default function Inventory() {
   return (
-    <Container maxWidth="lg" sx={{ marginTop: "3rem" }}>
-      <Search />
-      <SortFilter />
-      <Table />
-    </Container>
+    <>
+      <Container maxWidth="lg" sx={{ marginTop: "3rem" }}>
+        <Search />
+        <SortFilter />
+        <Table />
+      </Container>
+      <Box
+        sx={{
+          position: "sticky",
+          bottom: 0,
+          backgroundColor: "#0D0D0F",
+          py: 1,
+          pb: 2,
+          px: 3,
+        }}
+      >
+        <Typography
+          variant="body2"
+          color="#808087"
+          sx={{
+            fontSize: ".8rem",
+            mt: 3,
+          }}
+        >
+          Page 1 of total 1 pages.
+        </Typography>
+      </Box>
+    </>
   );
 }
--- recon/src/features/technologies/Technologies.tsx
+++ recon/src/features/technologies/Technologies.tsx
@@ -1,21 +1,44 @@
 import CollapsibleTable from "./Table";
 import Search from "../../ui/Search";
-import { Container } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 const formStyle: React.CSSProperties = {
   width: "100%",
 };
 
 export default function Technologies() {
   return (
-    <Container maxWidth="lg" sx={{ marginTop: "3rem" }}>
-      <Search
-        handleSubmit={(e) => {
-          e.preventDefault();
+    <>
+      <Container maxWidth="lg" sx={{ marginTop: "3rem" }}>
+        <Search
+          handleSubmit={(e) => {
+            e.preventDefault();
+          }}
+          formStyle={formStyle}
+          placeholder="Search your technologies"
+        />
+        <CollapsibleTable />
+      </Container>
+      <Box
+        sx={{
+          position: "sticky",
+          bottom: 0,
+          backgroundColor: "#0D0D0F",
+          py: 1,
+          pb: 2,
+          px: 3,
         }}
-        formStyle={formStyle}
-        placeholder="Search your technologies"
-      />
-      <CollapsibleTable />
-    </Container>
+      >
+        <Typography
+          variant="body2"
+          color="#808087"
+          sx={{
+            fontSize: ".8rem",
+            mt: 3,
+          }}
+        >
+          Page 1 of total 1 pages.
+        </Typography>
+      </Box>
+    </>
   );
 }
